fba22c6c028f1a8d0bd03803080f74080b226a31
Jeremy Christopher <jeremynchristopher@gmail.com>
2023-06-27 21:08:43 +0700

    Commit lagi, ternyata tadi bkn di root folder

--- BLINK/Features/BSD Link/Presenters/Components/PopUpHalteComponent.swift
+++ BLINK/Features/BSD Link/Presenters/Components/PopUpHalteComponent.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct PopUpHalteComponent: View {
-    @State private var minutes: Int = 0
-    var halte:[String] = ["Albera", "Allevare", "Ammarila Lavanya", "Astra", "Auto Part", "BCA", "Bursa Mobil", "Casa de Parco", "CBD", "Chadnya", "Club House Avani", "Collinare", "Cosmo", "De Brassia", "De Frangipani", "De Heliconia 1", "De Heliconia 2", "De Maja", "De Nara", "Devina and Desnha", "East Business District", "Edutown", "Eka Hospital 1", "Eka Hospital 2", "Eternity", "Extreme Park", "FBL 1", "FBL 2", "FBL 5", "Fiore", "Foglio", "Foresta 1", "Foresta 2", "Fresco", "Giant", "Giardina" ]
+    var halte:[String] = ["", "Albera", "Allevare", "Ammarila Lavanya", "Astra", "Auto Part", "BCA", "Bursa Mobil", "Casa de Parco", "CBD", "Chadnya", "Club House Avani", "Collinare", "Cosmo", "De Brassia", "De Frangipani", "De Heliconia 1", "De Heliconia 2", "De Maja", "De Nara", "Devina and Desnha", "East Business District", "Edutown", "Eka Hospital 1", "Eka Hospital 2", "Eternity", "Extreme Park", "FBL 1", "FBL 2", "FBL 5", "Fiore", "Foglio", "Foresta 1", "Foresta 2", "Fresco", "Giant", "Giardina" ]
     
-    @State private var selectedHalte: String = ""
+    @Binding var selectedHalte:String
     
-    let didClose: ()-> Void
-
+//    @Binding var flag:Int
+//    @Binding var pickUp: String
+//    @Binding var Destination: String
+//    @Binding var Time: String
+    
+    
+//    let didClose: ()-> Void
+    
 
     var body: some View {
         VStack(alignment: .center,spacing: 0){
@@ -34,18 +39,16 @@ struct PopUpHalteComponent: View {
                     .frame(maxWidth: .infinity)
                     .padding([.top,. bottom], 35)
                     .background(.white)
-
             }
         }
         .background(.white)
         .transition(.move(edge: .bottom))
-        
     }
 }
 
 struct PopUpHalteComponent_Previews: PreviewProvider {
     static var previews: some View {
-        PopUpHalteComponent{}
+        PopUpHalteComponent(selectedHalte: Binding.constant("Breeze"))
             .previewLayout(.sizeThatFits)
     }
 }
--- BLINK/Features/BSD Link/Presenters/Components/PopUpTimeComponent.swift
+++ BLINK/Features/BSD Link/Presenters/Components/PopUpTimeComponent.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct PopUpTimeComponent: View {
+    var schedule:[String] = ["", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
+    @Binding var time:String
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center,spacing: 0){
+            Picker("Selected Halte", selection: $time) {
+                ForEach(schedule, id: \.self) { halteName in
+                           Text(halteName)
+                    }
+            }
+            .frame(maxWidth: .infinity)
+            .clipped()
+            .pickerStyle(.inline)
+            .overlay(alignment: .top) {
+                Text("Starting Bus Stop")
+                    .font(.system(size: 17, weight: .semibold))
+                    .frame(maxWidth: .infinity)
+                    .padding([.top,. bottom], 35)
+                    .background(.white)
+            }
+        }
+        .background(.white)
+        .transition(.move(edge: .bottom))
     }
 }
 
 struct PopUpTimeComponent_Previews: PreviewProvider {
     static var previews: some View {
-        PopUpTimeComponent()
+        PopUpTimeComponent(time: Binding.constant("sad"))
     }
 }
